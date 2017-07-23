3ed50ad2390ed889c3de2cf3bc7440dd77dbc546
Logan Baker <lobaker13@gmail.com>
2017-07-22 23:43:31 -0400

    keyword enums edited

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,5 +1,4 @@
 class Keyword < ApplicationRecord
-  enum category: [:medium, :period, :movement]
   enum category: [:medium, :base, :period, :movement]
   has_many :art_keywords
   has_many :arts, through: :art_keywords
